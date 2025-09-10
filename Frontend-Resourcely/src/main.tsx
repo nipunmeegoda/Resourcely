948d5015f1015822ea5382162c88edad8db82964
nipunmeegoda <nipun.meegoda@gmail.com>
2025-09-10 12:35:39 +0530

    react-router-dom navigation applied

--- Frontend-Resourcely/src/main.tsx
+++ Frontend-Resourcely/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import './styles/global.css';
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+);
+
+
+
+
+
+
