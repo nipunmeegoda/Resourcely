db9d89321852b89afa3cdefa8e96a5096d26f537
Janidu Pabasara <141099578+JaniduP2003@users.noreply.github.com>
2025-09-10 20:36:01 +0530

    Check :: if the reactor dom works

---
+++ Frontend-Resourcely/src/pages/SignUp.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SignUp: React.FC = () => {
+  return (
+    <div>
+      <h1>Sign Up Page</h1>
+      <p>This is where new users can create an account.</p>
+    </div>
+  );
+};
+
+export default SignUp;
