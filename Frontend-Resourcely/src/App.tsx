ba981d196eaa7b8fdf94aa809d53d3307c7e50ae
it23194380 <it23194380@my.sliit.lk>
2025-09-10 19:56:09 +0530

    Setup route for BookingPage

--- Frontend-Resourcely/src/App.tsx
+++ Frontend-Resourcely/src/App.tsx
@@ -1,16 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import AdminPage from "./app/admin/page.tsx";
 import UserPage from "./app/user/page.tsx";
+import BookingPage from "./app/bookingForm/page.tsx";
+
 function App() {
-
-    return (
-        <Routes>
-            <Route path="/" element={<UserPage />} />
-            <Route path="/admin" element={<AdminPage />} />
-            <Route path="*" element={<h1>404 Not Found</h1>} />
-        </Routes>
-    );
-
+  return (
+    <Routes>
+      <Route path="/" element={<UserPage />} />
+      <Route path="/admin" element={<AdminPage />} />
+      <Route path="*" element={<h1>404 Not Found</h1>} />
+      <Route path="/booking" element={<BookingPage />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
