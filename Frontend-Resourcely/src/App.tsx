1334debaffd38579b4f69b0f6adbf54418926245
Janidu Pabasara <141099578+JaniduP2003@users.noreply.github.com>
2025-09-11 18:09:37 +0530

    ADD :: other code and everything works

--- Frontend-Resourcely/src/App.tsx
+++ Frontend-Resourcely/src/App.tsx
@@ -1,6 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import AdminPage from "./app/admin/page.tsx";
 import UserPage from "./app/user/page.tsx";
+
+import LoginPage from "./pages/LoginPage";
+import SignUp from "./pages/SignUp";
+import "./index.css"; // or App.css if you prefer
+
 function App() {
 
     return (
@@ -8,6 +13,8 @@ function App() {
             <Route path="/" element={<UserPage />} />
             <Route path="/admin" element={<AdminPage />} />
             <Route path="*" element={<h1>404 Not Found</h1>} />
+            <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUp />} />
         </Routes>
     );
 
