7ede0b99622128c295dde520a3053d6490e26272
Janidu Pabasara <141099578+JaniduP2003@users.noreply.github.com>
2025-09-14 16:20:56 +0530

    Error Fix ::: System.InvalidOperationException: Cannot create a DbSet for 'IdentityRole' because this type is not included in the model for the context.
    data folder dose not include the roles
    
    public class AppDbContext : DbContext  THIS ONLY KNOWS HOWTO USE CUSTOM ENTITTYS IT DOSE NOT KNOW HOW TO USE PREBUILD ONES IN FRAMEWORK
    
    so make it also handle Identity tables. one are pre  build in framework

--- Backend-Resourcely/Backend-Resourcely/Data/AppDbContext.cs
+++ Backend-Resourcely/Backend-Resourcely/Data/AppDbContext.cs
@@ -1,9 +1,11 @@
 using Backend_Resourcely.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Backend_Resourcely.Data
 {
-    public class AppDbContext : DbContext
+   public class AppDbContext : IdentityDbContext<ApplicationUser, IdentityRole, string>   //added the new roles
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
@@ -12,6 +14,7 @@ namespace Backend_Resourcely.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder); // Important for Identity
+
             modelBuilder.Entity<Booking>(entity =>
             {
                 entity.HasKey(b => b.Id);
