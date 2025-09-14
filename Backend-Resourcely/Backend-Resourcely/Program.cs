295a382398bbebf697487e8da2270d9f490fedaf
Janidu Pabasara <141099578+JaniduP2003@users.noreply.github.com>
2025-09-14 11:21:05 +0530

    ADD ::::: default admin user and added roles predefined and if condition to stop form making duplicates
    ASP.NET Core uses  dependency injection (DI) so used scope
    using ensures that when done everything is cleend up
    
    RoleManager → the tool to create, check, and manage roles
    UserManager → the tool to create and manage users in Identity.
    
    roles first and user next
    add if for stop form duplication

--- Backend-Resourcely/Backend-Resourcely/Program.cs
+++ Backend-Resourcely/Backend-Resourcely/Program.cs
@@ -38,6 +38,13 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+// Initialize roles and admin user  WHY THIS -- becouse hen the app runs for the first time, don’t have to manually create roles or the first admin. auto makes it 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;  // add scppe for DI 
+    await InitializeRolesAndAdmin(services);
+}
+
 app.MapGet("/", () => "Hello World! Backend is working! 🎉");
 
 // Ensure DB exists (dev)
@@ -56,6 +63,44 @@ if (app.Environment.IsDevelopment())
 
 //app.UseHttpsRedirection();
 app.UseCors();
+app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
+
+async Task InitializeRolesAndAdmin(IServiceProvider serviceProvider)
+{
+    var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();    //to manage  role a tool  like thing
+    var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();   //to manage users a tool 
+
+    // Create roles
+    string[] roleNames = { "Admin", "Manager", "User" };  // all roles i will be using 
+    foreach (var roleName in roleNames)
+    {
+        var roleExist = await roleManager.RoleExistsAsync(roleName);
+        if (!roleExist)  // if to see if the role is made or not 
+        {
+            await roleManager.CreateAsync(new IdentityRole(roleName)); //if not create it 
+        }
+    }
+
+    // Create admin user
+    var adminUser = await userManager.FindByEmailAsync("admin@example.com");
+    if (adminUser == null)
+    {
+        var user = new ApplicationUser    // defult admin user 
+        {
+            UserName = "admin@example.com",
+            Email = "admin@example.com",
+            FirstName = "Admin",
+            LastName = "User",
+            EmailConfirmed = true
+        };
+        
+        var createPowerUser = await userManager.CreateAsync(user, "Admin@123");
+        if (createPowerUser.Succeeded)                                                // ceack if admin is alrady made or not 
+        {
+            await userManager.AddToRoleAsync(user, "Admin");                            //if not make him 
+        }
+    }
+}
